0e6815c2d4df6e74cd103a57e8b5616b56e9d1be
GTIGit27 <puga.pascual@free.fr>
2015-12-03 13:46:29 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Envoyer un fichier</title>
+    </head> 
+        <body>
+            <form method="POST" enctype="multipart/form-data">
+                <input type="file" name="ma_photo">
+                <input type="submit" value="Envoyer">
+            </form>
+        </body>
+       
+</html>
+
+
+
